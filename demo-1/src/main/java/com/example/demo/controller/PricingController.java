c22a2b7be19f5c6ee8489e2c7b1794dc2a49941b
venkatashanmukhagit <shanmukharoyal475@gmail.com>
2022-06-22 13:05:06 +0530

    first

---
+++ demo-1/src/main/java/com/example/demo/controller/PricingController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PricingController {
+	
+	
+	@GetMapping("/price")
+	 public float getPrice() {
+		return 10000;
+		
+	}
+	
+
+}
